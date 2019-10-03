013ff6dc4f96f7b615ff9b92d098955e56a769b8
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-10-02 23:20:35 -0300

    Configure routes

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Spot from './pages/Spot';
+
+export default () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/dashboard" exact component={Dashboard} />
+      <Route path="/spot" exact component={Spot} />
+    </Switch>
+  </BrowserRouter>
+);
