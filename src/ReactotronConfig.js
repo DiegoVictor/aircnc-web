ef16758effa0f934559d21716704ada2be202925
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-12-28 18:43:27 -0300

    Move configuration to its respective folder

--- src/ReactotronConfig.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import Reactotron from 'reactotron-react-js';
-
-if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure().connect();
-  tron.clear();
-}
