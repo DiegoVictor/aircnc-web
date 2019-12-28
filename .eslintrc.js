e139fefdbba26f7363e79f62f5761d358ef9822b
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-12-28 18:42:32 -0300

    Config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,9 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    jest: true,
   },
-  extends: [
-    'airbnb', 'prettier', 'prettier/react'
-  ],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -18,28 +17,23 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react', 'prettier', 'react-hooks'
-  ],
+  plugins: ['react', 'prettier', 'react-hooks'],
   rules: {
     'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [
-      'error',
-      { extensions: ['.js', '.jsx'] }
-    ],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
     'import/prefer-default-export': 'off',
     'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'jsx-a11y/label-has-associated-control': 'off',
     'no-underscore-dangle': 'off',
-    'camelcase': 'off'
+    camelcase: 'off',
   },
   settings: {
     'import/resolver': {
       'babel-plugin-root-import': {
-        rootPathSuffix: 'src'
-      }
-    }
-  }
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
