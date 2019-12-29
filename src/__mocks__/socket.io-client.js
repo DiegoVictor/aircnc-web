5d4e670e1ab8e3324ab305f8801c165d5845e367
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-12-29 14:50:25 -0300

    Mock socket

---
+++ src/__mocks__/socket.io-client.js
@@ -0,0 +1,20 @@
+export const emit = jest.fn();
+
+export const to = jest.fn(() => {
+  return { emit };
+});
+
+let callback;
+export const run = socket => {
+  callback(socket);
+};
+
+export const on = jest.fn((event, cb) => {
+  callback = cb;
+});
+
+export default () => ({
+  to,
+  emit,
+  on,
+});
