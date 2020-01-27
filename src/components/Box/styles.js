8c02b60919e31d40f3c63bd0909f6d6d0f1fbe96
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-01-27 17:27:45 -0300

    Update css

--- src/components/Box/styles.js
+++ src/components/Box/styles.js
@@ -21,6 +21,7 @@ export const Container = styled.div`
       font-size: 14px;
       font-weight: bold;
       margin-bottom: 8px;
+      margin-top: 15px;
 
       span {
         color: #999;
@@ -29,13 +30,22 @@ export const Container = styled.div`
       }
     }
 
+    span {
+      color: #f05a5b;
+      margin-bottom: 5px;
+      margin-top: 5px;
+    }
+
     input {
       border: 1px solid #ddd;
       border-radius: 2px;
       font-size: 16px;
       height: 45px;
       padding: 0px 15px;
-      margin-bottom: 20px;
+    }
+
+    button {
+      margin-top: 20px;
     }
   }
 `;
