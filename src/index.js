63bb1334804fbb6cee941881b9d67d6929bde2c2
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-08 13:16:25 -0300

    Start the app from routes

--- src/index.js
+++ src/index.js
@@ -2,16 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '~/config/ReactotronConfig';
-import Default from '~/components/pages';
-import Style from '~/styles';
-import Routes from '~/routes';
+import Navigation from '~/routes';
 
-ReactDOM.render(
-  <>
-    <Style />
-    <Default>
-      <Routes />
-    </Default>
-  </>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Navigation />, document.getElementById('root'));
