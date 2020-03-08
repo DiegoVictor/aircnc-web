6861e3f4b5287024e5efa85ee99c9a6e9f3e057d
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-08 13:15:45 -0300

    Simplify component

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import { Container } from './styles';
+export default styled.div`
+  background-color: #fff;
+  border-radius: 4px;
+  padding: 30px;
+  width: 100%;
 
-export default function Box({ children }) {
-  return <Container>{children}</Container>;
-}
+  > p {
+    font-size: 22px;
+    line-height: 30px;
+    margin-bottom: 30px;
+  }
 
-Box.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.array, PropTypes.element])
-    .isRequired,
-};
+  form {
+    display: flex;
+    flex-direction: column;
+
+    label {
+      color: #444;
+      font-size: 14px;
+      font-weight: bold;
+      margin-bottom: 8px;
+      margin-top: 15px;
+
+      span {
+        color: #999;
+        font-size: 12px;
+        font-weight: normal;
+      }
+    }
+
+    span {
+      color: #f05a5b;
+      margin-bottom: 5px;
+      margin-top: 5px;
+    }
+
+    input {
+      border: 1px solid #ddd;
+      border-radius: 2px;
+      font-size: 16px;
+      height: 45px;
+      padding: 0px 15px;
+    }
+
+    button {
+      margin-top: 20px;
+    }
+  }
+`;
