126b1815062207706b69bd803f736e4ea99363da
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-01-22 18:46:17 -0300

    Set token on axios after load from storage

--- src/contexts/User/index.js
+++ src/contexts/User/index.js
@@ -1,8 +1,11 @@
 import { createContext } from 'react';
 
+import { setAuthorization } from '~/services/api';
+
 let user = {};
 if (typeof localStorage.aircnc_user !== 'undefined') {
   user = JSON.parse(localStorage.getItem('aircnc_user'));
+  setAuthorization(user.token);
 }
 
 export const UserContext = createContext(user);
