af1d5b001dd5a6817ec90eaa2bf933940f8b254d
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-08-17 12:58:24 -0300

    Add SVG module mock for Jest testing

--- jest.config.js
+++ jest.config.js
@@ -85,6 +85,7 @@ module.exports = {
   // A map from regular expressions to module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '^~/(.*)': '<rootDir>/src/$1',
+    '\\.(svg)$': '<rootDir>/tests/mocks/file.js',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
